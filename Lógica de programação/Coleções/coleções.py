cc7d0b4ccc446803f89d00201b0151154ae8b4e1
ArthurdevjuniorIA <kaua.arthur@escolar.ifrn.edu.br>
2026-06-11 10:59:34 -0300

    Atualizando arquivos da pasta

--- Lógica de programação/Coleções/coleções.py
+++ Lógica de programação/Coleções/coleções.py
@@ -1,4 +1,4 @@
-nomes = ["Arthur", "Tutu", "Roberto", "Maria", "Clara", "Te amo", "Clara", "Maria", "Clara"]
+nomes = ["Arthur", "Tutu", "Roberto", "Maria", "Clara", "Luiz", "Maria", "Mario"]
 print(nomes.count("Clara"))
 if "Maria" and "Clara" in nomes:
     print("Maria Clara is so pretty")
