a5c5d5fc0d42532d5675347f401da3d7ddc1859b
Arthur Kauã <kaua.arthur@escolar.ifrn.edu.br>
2026-06-30 20:47:07 -0300

    More problems

--- Lógica de programação/Laços de repetição/Outros problemas laços de repetição/percorrendo.py
+++ Lógica de programação/Laços de repetição/Outros problemas laços de repetição/percorrendo.py
@@ -2,7 +2,23 @@
 nome = clientes
 nome = " ".join(nome)
 print(nome)'''
-valores = [10, 20, 30, 40, 50]
+'''valores = [10, 20, 30, 40, 50]
+soma = 0
 for valor in valores:
-    valor = valor+ valor
-print(valor)
+    soma = soma+valor
+print(soma)'''
+projetos = ["website", "jogo", "análise de dados", None, "aplicativo móvel"]
+'''for none in projetos:
+    if none is None:
+        print("Projeto ausente!")
+    else:
+        print(none)'''
+'''livros = ["1984", "Dom Casmurro", "O Pequeno Príncipe", "O Hobbit", "Orgulho e Preconceito"]
+for hobbit in livros:
+    if hobbit == "O Hobbit":
+        print("Livro encontrado: O hobbit")
+        break'''
+for i in range(4,-1,-1):
+    print(f"Venda realizada! Estoque restante: {i}")
+    if i == 0:
+        print("Estoque acabou")
