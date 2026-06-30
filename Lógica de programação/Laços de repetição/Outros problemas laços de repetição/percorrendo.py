a945a1f9ffd886e6d41732756e8000c1d91b0976
Arthur Kauã <kaua.arthur@escolar.ifrn.edu.br>
2026-06-30 19:59:04 -0300

    Practing

---
+++ Lógica de programação/Laços de repetição/Outros problemas laços de repetição/percorrendo.py
@@ -0,0 +1,8 @@
+''''clientes = ["João", "Maria", "Carlos", "Ana", "Beatriz"]
+nome = clientes
+nome = " ".join(nome)
+print(nome)'''
+valores = [10, 20, 30, 40, 50]
+for valor in valores:
+    valor = valor+ valor
+print(valor)
