79467953ff3afbe53738fa0205288e6c9f96c619
Arthur Kauã <kaua.arthur@escolar.ifrn.edu.br>
2026-07-03 19:54:12 -0300

    Solving problems

--- OBI 2026.py/super_desconto.py
+++ OBI 2026.py/super_desconto.py
@@ -1,17 +1,20 @@
 quantidade_produtos = int(input())
 frete_padrao = 20
 soma_produto = 0
+apenas_um = 0
 for i in range(quantidade_produtos):
     valor_produto = float(input())
+    tem_brinde = valor_produto == 0
     if valor_produto<0:
         continue
     soma_produto = valor_produto + soma_produto
-    if valor_produto == 0:
-        frete_padrao = 20
-        soma_produto = soma_produto + frete_padrao
-if soma_produto>=150 and valor_produto != 0:
+    if tem_brinde == True:
+        apenas_um +=1
+        if apenas_um>=2:
+            continue
+if soma_produto>=150 and apenas_um == 0:
     frete_padrao = 0
     soma_produto = soma_produto + frete_padrao
-elif soma_produto<150:
+else:
     soma_produto = soma_produto + frete_padrao
-print(f"Total = {soma_produto} R$")
+print(f"Total: R$ {soma_produto:.2f}")
