30b69d0fa15f7ad6948ffe4f33b6b3e09338a1cf
Arthur Kauã <kaua.arthur@escolar.ifrn.edu.br>
2026-07-05 12:00:44 -0300

    Atualizando

--- Lógica de programação/Listas,  tuplas e Arrays/triagem.py
+++ Lógica de programação/Listas,  tuplas e Arrays/triagem.py
@@ -1,2 +1,12 @@
 nome = input("Digite o seu nome: ")
 idade = int(input("Digite sua idade: "))
+atendimento_prioritario = [ ]
+atendimento_normal = [ ]
+if idade>=60:
+    atendimento_prioritario.append(idade)
+else:
+    atendimento_normal.append(idade)
+for i in range(len(atendimento_normal)):
+    for idoso in atendimento_prioritario:
+        if atendimento_prioritario == [ ]:
+            print(atendimento_normal[1-i])
