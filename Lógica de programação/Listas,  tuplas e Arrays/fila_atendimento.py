df2267700a6ac6e1fa30de444eec48637de97ecd
Arthur Kauã <kaua.arthur@escolar.ifrn.edu.br>
2026-07-09 14:05:23 -0300

    feat: Finishing my file training data structure

--- Lógica de programação/Listas,  tuplas e Arrays/fila_atendimento.py
+++ Lógica de programação/Listas,  tuplas e Arrays/fila_atendimento.py
@@ -5,7 +5,10 @@ while True:
         fila_atendimento.append(cliente_comando)
     else:
         print(f"Próximo cliente: {fila_atendimento[0]}")
+        fila_atendimento.pop(0)
     if cliente_comando == "fim":
+        fila_atendimento.remove("fim")
         print("=== CLIENTES AGUARDANDO ATENDIMENTO ===")
         for pessoas in fila_atendimento:
             print(pessoas)
+        break
