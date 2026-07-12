d0ce3bfd270e4bf4fc17252e2a4c97ed0a1b1e55
Arthur Kauã <kaua.arthur@escolar.ifrn.edu.br>
2026-07-12 19:21:06 -0300

    feat: Solving more problems about lists in python

---
+++ Lógica de programação/Listas,  tuplas e Arrays/equipes_limpeza.py
@@ -0,0 +1,10 @@
+equipes_de_3 = [ ]
+while True:
+    voluntario = input("Nome do voluntário (ou 'fim' para encerrar): ")
+    if voluntario  == "fim":
+        break
+    else:
+        equipes_de_3.append(voluntario)
+        for i in range(len(equipes_de_3) == 3):
+             equipes_de_3.remove[i]
+             
