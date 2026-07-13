ef98197516b017983adffdb4a7679ebe02def6b2
Arthur Kauã <kaua.arthur@escolar.ifrn.edu.br>
2026-07-13 19:34:37 -0300

    feat: solving another problem about lists

---
+++ Lógica de programação/Listas,  tuplas e Arrays/sensor_defeituoso.py
@@ -0,0 +1,19 @@
+temperaturas_validas = [ ]
+while True:
+    temperatura = float(input("Digite a temperatura (999 para encerrar): "))
+    if temperatura == 999:
+        for i in range(len(temperaturas_validas)):
+            soma = 0
+            some = temperaturas_validas[i]
+            soma = some + soma
+        divide = some/len(temperaturas_validas)
+        print("Temperaturas válidas: ")
+        for valido in temperaturas_validas:
+            print(valido)
+        print(f"Média das temperaturas: {divide:.2f}°C")
+        break
+    else:
+        if temperatura>50 or temperatura<-20:
+            continue
+        temperaturas_validas.append(temperatura)
+    
