3131329186e5f9b4104cb7898b01267788a667e8
Arthur Kauã <kaua.arthur@escolar.ifrn.edu.br>
2026-07-13 13:00:11 -0300

    feat: solving the problem

--- Lógica de programação/Listas,  tuplas e Arrays/equipes_limpeza.py
+++ Lógica de programação/Listas,  tuplas e Arrays/equipes_limpeza.py
@@ -1,10 +1,18 @@
 equipes_de_3 = [ ]
+equipes = [ ]
 while True:
     voluntario = input("Nome do voluntário (ou 'fim' para encerrar): ")
     if voluntario  == "fim":
+        for i in range(len(equipes)):
+                print(f"Equipe {i}")
+                print(*equipes[i])
         break
+        
     else:
         equipes_de_3.append(voluntario)
-        for i in range(len(equipes_de_3) == 3):
-             equipes_de_3.remove[i]
-             
+        if len(equipes_de_3) == 3:
+                equipes.append(equipes_de_3)
+                equipes_de_3.copy()
+
+           
+
