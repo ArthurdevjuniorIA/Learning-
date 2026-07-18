b2d1e8ffb0b7994b1dc81c181dad0f0aa82447e5
Arthur Kauã <kaua.arthur@escolar.ifrn.edu.br>
2026-07-18 20:28:47 -0300

    feat: Solving another problem

---
+++ Lógica de programação/Listas,  tuplas e Arrays/carrinho_de_compras.py
@@ -0,0 +1,8 @@
+carrinho = [ ]
+while True:
+    produto = input("Digite qual produto você deseja colocar no seu carrinho: ").lower()
+    if produto not in carrinho:
+        print(f"{produto} adicionado ao carrinho")
+    else:
+        carrinho.remove(produto)
+        print(f"O {produto} já está no carrinho")
