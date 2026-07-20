796b9be24c65ec8091e7974621f76779d89779e8
Arthur Kauã <kaua.arthur@escolar.ifrn.edu.br>
2026-07-20 19:58:14 -0300

    trying to solve this real problem

--- Lógica de programação/Listas,  tuplas e Arrays/relatorio_produção_fábrica.py
+++ Lógica de programação/Listas,  tuplas e Arrays/relatorio_produção_fábrica.py
@@ -1,16 +1,18 @@
 nome_funcionario = [ ]
-producao_lista = [ ]
+soma_producao = 0
 while True:
     nome_do_funcionario = input("Nome do funcionário: ")
     if nome_do_funcionario == "fim":
         print("==============================\nRELATÓRIO GERAL DA SEMANA\n==============================")
-        soma_producao = sum(producao_lista)
-        print(soma_producao)
+        for some in producao:
+            soma_producao = some+soma_producao
+        media_producao = soma_producao/len(producao)
+        print(f"Total de peças montadas: {soma_producao}")
+        print(f"Média de produção por funcionário: {media_producao:.0f}")
         break
-    producao = map(int,input("Produção (Seg Ter Qua Qui Sex): ").split())
+    producao = list(map(int,input("Produção (Seg Ter Qua Qui Sex): ").split()))
     if nome_do_funcionario not in nome_funcionario:
         nome_funcionario.append(nome_do_funcionario)
-        producao_lista.append(producao)
 
     else:
         nome_funcionario.remove(nome_funcionario)
