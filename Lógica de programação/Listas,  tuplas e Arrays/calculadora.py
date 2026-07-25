d452131ba6ad2a9b7f1e15a0b904226757bfc5e5
Arthur Kauã <kaua.arthur@escolar.ifrn.edu.br>
2026-07-25 16:36:31 -0300

    fix: Resolving the bugs

--- Lógica de programação/Listas,  tuplas e Arrays/calculadora.py
+++ Lógica de programação/Listas,  tuplas e Arrays/calculadora.py
@@ -1,4 +1,21 @@
+from unidecode import unidecode
 
-a = float(input())
-potenciacao = a**2
-print(potenciacao)
+pergunta = int(input("Quantas variáveis você quer calcular? "))
+soma = 0
+subtracao = 0
+multiplicacao = 1
+divisao = 1
+potenciacao = 1
+radiciacao = 2
+for i in range(pergunta):
+    numero = float(input("Digite o numero(s): "))
+    operacao = input("qual operação você deseja realizar? ")
+    operacao_limpa = unidecode(operacao)
+
+    if operacao_limpa == "soma":
+        soma = numero+soma
+    elif operacao_limpa == "subtracao":
+        subtracao = numero-subtracao
+    elif operacao_limpa == "multiplicacao":
+        multiplicacao = numero*multiplicacao
+
