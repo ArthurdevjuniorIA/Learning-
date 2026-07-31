c4aa656ff125e26124161d7532f298497ec000c1
Arthur Kauã <kaua.arthur@escolar.ifrn.edu.br>
2026-07-31 19:09:32 -0300

    feat: Studying binary code

---
+++ Lógica de programação/Listas,  tuplas e Arrays/listas_tuplas_sets.py
@@ -0,0 +1,8 @@
+lista = ["Isso", "É", "uma", "lista","bem","opa","oh","my","god"]
+tupla = ("Isso", "É", "uma", "tupla")
+quebre = "hello world".split()
+conjunto = {"Isso","Isso"}
+print(*lista)
+print(*tupla)
+print(*conjunto)
+print(quebre[0b10])
