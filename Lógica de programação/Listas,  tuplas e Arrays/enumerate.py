d4fec9e5b08de27b76c73d675e83eca55ffa6e1d
Arthur Kauã <kaua.arthur@escolar.ifrn.edu.br>
2026-08-03 19:38:47 -0300

    feat: trying solving problems about leetcode

---
+++ Lógica de programação/Listas,  tuplas e Arrays/enumerate.py
@@ -0,0 +1,3 @@
+saudacoes = ["Oi","bom dia","boa tarde","ola"]
+for index,saudacao in enumerate(saudacoes):
+    print(index,saudacao)
--- Lógica de programação/Listas,  tuplas e Arrays/listas_tuplas_sets.py
+++ Lógica de programação/Listas,  tuplas e Arrays/listas_tuplas_sets.py
@@ -1,8 +1,12 @@
-lista = ["Isso", "É", "uma", "lista","bem","opa","oh","my","god"]
+scientists = [ "Newton", "Einstein","Babbage","Planck", "Schoridinger","Turing"]
+computer_scientists = ["Ada Lovelace", "Von Neumann","Linus Torvalds","Claude Shannon"]
+numbers = [1,2,3,4,5,6,7,8,9]
 tupla = ("Isso", "É", "uma", "tupla")
 quebre = "hello world".split()
 conjunto = {"Isso","Isso"}
-print(*lista)
-print(*tupla)
-print(*conjunto)
-print(quebre[0b10])
+insert = scientists.extend(computer_scientists)
+print(*scientists)
+print(scientists[0b101])
+scientists.extend(numbers)
+print(scientists)
+print(numbers[::3])
