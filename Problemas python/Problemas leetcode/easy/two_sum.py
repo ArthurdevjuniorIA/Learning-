944a6bd7287d5f70c9263f4c80d7110b99deaea8
Arthur Kauã <kaua.arthur@escolar.ifrn.edu.br>
2026-08-06 18:42:51 -0300

    feat: Solving problems

--- Problemas python/Problemas leetcode/easy/two_sum.py
+++ Problemas python/Problemas leetcode/easy/two_sum.py
@@ -1,5 +1,10 @@
-enter,target = input()
+enter = input()
+target = int(input("target = "))
 nums = [ int(number)for number in enter.strip("[]").split(",")]
-for i in range(nums):
-    for k in range(nums):
-        
+for numero in nums:
+    for num in nums:
+        if numero+num == target:
+           index_1 = nums.index(numero)
+           index_2 = nums.index(num)
+    break
+print(f"[{index_1},{index_2}]")
